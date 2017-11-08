fb07bdc5612f25ff8ae40a10822840927ad37ea8
MJRLegends <MJRLegends@users.noreply.github.com>
2017-11-08 15:55:13 +0000

    Initial commit

---
+++ MJRLegends Backup Tool/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace MJRLegends_Backup_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSaveData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(450, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 0;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.Location = new System.Drawing.Point(5, 41);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(521, 303);
+            this.listBoxTasks.TabIndex = 1;
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnBackup.Location = new System.Drawing.Point(0, 483);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(533, 43);
+            this.btnBackup.TabIndex = 2;
+            this.btnBackup.Text = "Run Backup";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(120, 353);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(145, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtFolderName
+            // 
+            this.txtFolderName.Location = new System.Drawing.Point(314, 354);
+            this.txtFolderName.Name = "txtFolderName";
+            this.txtFolderName.ReadOnly = true;
+            this.txtFolderName.Size = new System.Drawing.Size(145, 20);
+            this.txtFolderName.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 357);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Folder";
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(463, 356);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(29, 19);
+            this.btnSelectFolder.TabIndex = 7;
+            this.btnSelectFolder.Text = "...";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(359, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "MJRLegends Backup Tool v1.0.0";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(5, 386);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.Size = new System.Drawing.Size(520, 91);
+            this.txtStatus.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(5, 351);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSaveData
+            // 
+            this.btnSaveData.Location = new System.Drawing.Point(372, 12);
+            this.btnSaveData.Name = "btnSaveData";
+            this.btnSaveData.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveData.TabIndex = 11;
+            this.btnSaveData.Text = "Save Data";
+            this.btnSaveData.UseVisualStyleBackColor = true;
+            this.btnSaveData.Click += new System.EventHandler(this.btnSaveData_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 526);
+            this.Controls.Add(this.btnSaveData);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtFolderName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.listBoxTasks);
+            this.Controls.Add(this.btnDelete);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(549, 565);
+            this.MinimumSize = new System.Drawing.Size(549, 565);
+            this.Name = "Form1";
+            this.Text = "MJRLegends Backup Tool v1.0.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.ListBox listBoxTasks;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtFolderName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSaveData;
+    }
+}
+
---
+++ MJRLegends Backup Tool/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.IO.Compression;
+using Ionic.Zip;
+
+namespace MJRLegends_Backup_Tool
+{
+    public partial class Form1 : DevComponents.DotNetBar.Metro.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XMLRead();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            String path = fbd.SelectedPath;
+            txtFolderName.Text = path;
+            btnSelectFolder.Enabled = true;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedIndex < 0)
+            {
+                MessageBox.Show("Please select a entry first", "ERROR!");
+            }
+            else
+            {
+                listBoxTasks.Items.Remove(listBoxTasks.SelectedItem.ToString());
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(txtName.Text != "" && txtFolderName.Text != "")
+            {
+                listBoxTasks.Items.Add(txtName.Text + " > " + txtFolderName.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please make sure all fields are filled out before adding a entry!", "ERROR!");
+            }
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            txtStatus.AppendText("Creating Backups!..." + "\n");
+            for (int i = 0; i < listBoxTasks.Items.Count; i++)
+            {
+                ZipFile zip = new ZipFile();
+                zip.ParallelDeflateThreshold = -1;
+                zip.CompressionLevel = Ionic.Zlib.CompressionLevel.Default;
+
+                String listItem = listBoxTasks.Items[i].ToString();
+                txtStatus.AppendText("Creating Backup for " + listItem.Substring(0, listItem.IndexOf(">") - 1) + "..." + "\n");
+                zip.AddDirectory(listItem.Substring(listItem.IndexOf(" > ") + 3));
+
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.InitialDirectory = @"C:\";
+                saveFileDialog1.Title = "Save Backup for " + listItem.Substring(0, listItem.IndexOf(">") - 1);
+                saveFileDialog1.CheckPathExists = true;
+                saveFileDialog1.DefaultExt = ".zip";
+                saveFileDialog1.Filter = "ZIP file (*.zip)|*.zip";
+                saveFileDialog1.FilterIndex = 1;
+                saveFileDialog1.FileName = listItem.Substring(0, listItem.IndexOf(">") - 1) + " " + DateTime.Now.Day.ToString() + "-" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Year.ToString() + "-" + DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString() + "-" + DateTime.Now.Second.ToString() + ".zip";
+                saveFileDialog1.RestoreDirectory = true;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    zip.Save(saveFileDialog1.FileName);
+                }
+
+                zip.Dispose();
+
+                txtStatus.AppendText("Backup for " + listItem.Substring(0, listItem.IndexOf(">") - 1) + " is now  Completed!..." + "\n");
+            }
+            txtStatus.AppendText("Backups Completed!..." + "\n");
+        }
+
+        private void savetoxml()
+        {
+            if (File.Exists(Application.StartupPath + "Data" + ".xml"))
+                File.Delete(Application.StartupPath + "Data" + ".xml");
+
+            using (XmlWriter writer = XmlWriter.Create(Application.StartupPath + "/Data" + ".xml"))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Entries");
+                writer.WriteAttributeString("NumOfEntries", "" + listBoxTasks.Items.Count);
+                for (int i = 0; i < listBoxTasks.Items.Count; i++)
+                {
+                    String listItem = listBoxTasks.Items[i].ToString();
+                    writer.WriteAttributeString("NameEntry" + i, listItem.Substring(0, listItem.IndexOf(">") - 1));
+                    writer.WriteAttributeString("FolderEntry" + i, listItem.Substring(listItem.IndexOf(" > ") + 3));
+                }
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Flush();
+                writer.Close();
+            }
+        }
+        private void XMLRead()
+        {
+            if (File.Exists(Application.StartupPath + "/Data" + ".xml"))
+            {
+                if (listBoxTasks.Items.Count == 0)
+                {
+                    XmlTextReader reader = new XmlTextReader(Application.StartupPath + "/Data" + ".xml");
+                    while (reader.Read())
+                    {
+                        if (reader.NodeType == XmlNodeType.Element)
+                        {
+                            if (reader.Name == "Entries")
+                            {
+                                for (int i = 0; i < Convert.ToInt32(reader.GetAttribute("NumOfEntries")); i++)
+                                {
+                                    listBoxTasks.Items.Add(reader.GetAttribute("NameEntry" + i) + " > " + reader.GetAttribute("FolderEntry" + i));
+                                }
+                            }
+                        }
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+        private void btnSaveData_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.Items.Count > 0)
+            {
+                savetoxml();
+            }
+            else
+                MessageBox.Show("Please make sure have more or more entries before saving!", "ERROR!");
+        }
+    }
+}
